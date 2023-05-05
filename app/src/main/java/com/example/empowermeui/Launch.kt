3ee79f40dd3f4814d9dc125795ecb1be1a070301
Imesh Pasinda <imeshpasinda@gmail.com>
2023-05-05 22:01:40 +0530

    Add Launch Activity

---
+++ app/src/main/java/com/example/empowermeui/Launch.kt
@@ -0,0 +1,24 @@
+package com.example.empowermeui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatDelegate
+
+class Launch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, MyProfile::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
